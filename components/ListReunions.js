b150c7599b43cf1bf224d425f0daffd4403aa1c0
Saly-Ba <sellyba7@gmail.com>
2020-05-18 06:05:39 +0000

    new update

--- components/ListReunions.js
+++ components/ListReunions.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, TouchableHighlight } from 'react-native';
 import firebase from '../firebase';
+import { Icon } from 'react-native-elements';
 import _ from 'lodash';
 
 
-export default function ListReunions(){
+export default function ListReunions({navigation}){
     const [liste, setListe] = useState();
     function getReunion(){
         firebase.database().ref('/reunion').on('value', function(snapshot) {
@@ -17,27 +18,58 @@ export default function ListReunions(){
             setListe(listOfR)   
         });
     }
+
+    function deleteReunion(key){
+        firebase.database().ref(`/reunion/${key}`).remove();
+    }
     return(
         <View style={styles.container}>
-            <View style={{flex:1}}>
+            <View style={styles.cardFlat}>
                 <FlatList
                     data={liste}
                     keyExtractor={(item)=>item.key}
                     renderItem={({item}) =>{
                         return(
-                            <View>
-                                <Text style={{color:'white'}}>{item.sujet}</Text>
-                                <Text style={{color:'white'}}>{item.dateTime}</Text>
-                                <Text style={{color:'white'}}>{item.lieu}</Text>
-                                <Text style={{color:'white'}}>{item.participants}</Text>
+                            <View style={styles.card}>
+                                    <View style={{flex:1, flexDirection:'row', justifyContent: 'space-between'}}>
+                                        <Text style={[styles.textCard, {fontSize:24, fontWeight:'bold'}]}>{item.sujet}</Text>
+                                        <Text style={[styles.textCard,{fontWeight:'bold'}]}>{item.dateTime}</Text>
+                                    </View>
+                                    <View style={{flex:1, flexDirection:'row', justifyContent: 'space-between'}}>
+                                        <View>
+                                            <Text style={[styles.textCard, {fontSize:20}]}>{item.lieu}</Text>
+                                            <Text style={styles.textCard}>{item.participants}</Text>
+                                        </View>
+                                        <View>
+                                            <Icon name='close'
+                                                //reverse
+                                                //raised
+                                                color='black'
+                                                type='material'
+                                                size={30}
+                                                onPress={()=>{deleteReunion(item.key)}}
+                                            />
+                                        </View>
+                                    </View>
+                                
                             </View>
                         )
                     }}
                 />
+            <Icon
+                reverse
+                raised
+                name='add'
+                type='material'
+                color='#76ff03'
+                //iconStyle={{width:100}}
+                size={40}
+                containerStyle={styles.addButton}
+                onPress={()=>{navigation.navigate("Ajouter une Reunion")}}
+            />    
             </View>
-            <View style={{flex:1}}>
-                <TouchableOpacity onPress={getReunion} style={styles.refresh}><Text style={{color:'white'}}>Click me!</Text></TouchableOpacity>
-            </View>
+            <TouchableOpacity onPress={getReunion} style={styles.refresh}><Text style={{color:'white'}}>Click me!</Text></TouchableOpacity>
+            
         </View>
     );
 }
@@ -48,9 +80,24 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       backgroundColor: '#000',
       //justifyContent: 'center',
-      //padding:20,
+      padding:10,
     },
-
+    card:{
+        flex:1, 
+        elevation:8,
+        borderRadius:20,
+        backgroundColor:'#00acc1',
+        padding:15,
+        marginBottom:20,
+        width: '100%',
+    },
+    cardFlat:{
+        flex:1,
+        width:'100%'
+    },
+    textCard:{
+        color:'white',
+    },
     refresh:{
         marginTop: 20,
         backgroundColor:'#006064',
@@ -60,5 +107,16 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    addButton:{
+        position: 'absolute',
+        bottom:60,
+        right:20,
+        width:100,
+        height:100,
+        backgroundColor:'#76ff03',
+        borderRadius:50,
+        alignItems:'center',
+        justifyContent:'center'
+    }
 
 });
